2cefc998d3e8fa3b0e3b8b1acb5eacc2f8d9ad13
zedin27 <ztisneshamzeh@csumb.edu>
2018-02-03 18:36:30 -0800

    created header

--- includes/libft.h
+++ includes/libft.h
@@ -6,15 +6,9 @@
 /*   By: ztisnes <ztisnes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 21:47:24 by ztisnes           #+#    #+#             */
-/*   Updated: 2018/02/03 17:35:01 by ztisnes          ###   ########.fr       */
+/*   Updated: 2018/02/03 18:35:41 by ztisnes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*TODO: Reorganize and make libft better and efficient
-**		Consider in creating obstack header
-**		restructure lists and other ADT
-**		Create unique files for stacks, queues, and binary trees
-*/
 
 #ifndef LIBFT_H
 # define LIBFT_H
